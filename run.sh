fea18ec4c7456f962b56b2a237170741e45866ba
frasan00 <francesco.sangiovanni.7@gmail.com>
2023-07-10 10:11:13 +0200

    small changes

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 # install gnu compiler if not already installed
 # sudo pacman -S gcc // installation for arch linux
+# sudo apt-get update && sudo apt-get install -y build-essential on ubuntu
 
 g++ ./src/main.cpp -o server.exe
 
